4a199dcd5906c671dd3f30b98cdbfd86dd151963
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 16:16:32 -0600

    cotizador

---
+++ app/protected/cotizador/page.tsx
@@ -0,0 +1,13 @@
+import SideBarAdmin from "../elementos/sidebar/sidebar_admin/sidebar_admin";
+
+
+
+export default function Home() {
+    return (
+      
+      <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
+        <SideBarAdmin/>
+       
+      </main>   
+    );
+  }
