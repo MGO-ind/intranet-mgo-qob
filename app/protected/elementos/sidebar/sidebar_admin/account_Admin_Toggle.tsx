7f8932221b4422708a27129a4357ec71db671eb9
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 14:05:57 -0600

    pading en eje x

--- app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
+++ app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
@@ -7,7 +7,7 @@ export const AccountAdminToggle = async () => {
         <div className="border-b mb-4 mt-2 pb-4 border-stone-200">
             <button className="flex p-0.5 hover:bg-stone-600 rounded-full transition-colors relative gap-2 w-full item-center">
         
-                <div className="text-center px-1">
+                <div className="text-center px-2">
                     <span className="text-sm font-bold block">
                         name
                     </span> 
