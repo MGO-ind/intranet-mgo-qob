c2174e49e1affdb8491f1dcc6e15d291d27b3688
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 10:26:45 -0600

    sidebar en la pagina principal

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,15 +1,16 @@
 import { auth, signOut } from 'app/auth';
+import SideBarAdmin from './elementos/sidebar/sidebar_admin/sidebar_admin';
 
 export default async function ProtectedPage() {
   let session = await auth();
 
   return (
-    <div className="flex h-screen bg-black">
-      <div className="w-screen h-screen flex flex-col space-y-5 justify-center items-center text-white">
-        You are logged in as {session?.user?.email}
-        <SignOut />
-      </div>
-    </div>
+    <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
+    <SideBarAdmin/>
+
+
+   <SignOut />
+  </main> 
   );
 }
 
