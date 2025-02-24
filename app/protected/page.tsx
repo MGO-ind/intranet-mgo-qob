90152536626da3fc96f845f4937a44a815127a47
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 11:10:03 -0600

    inicio

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,6 @@
 import { auth, signOut } from 'app/auth';
 import SideBarAdmin from './elementos/sidebar/sidebar_admin/sidebar_admin';
+import { Dashboard } from './elementos/dashboard/dashboard';
 
 export default async function ProtectedPage() {
   let session = await auth();
@@ -7,6 +8,7 @@ export default async function ProtectedPage() {
   return (
     <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
     <SideBarAdmin/>
+    <Dashboard />
 
 
    <SignOut />
