46c1a3639ce3266e259f366c617d847f2ba48503
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 11:09:10 -0600

    pagina de inicio

---
+++ app/protected/elementos/dashboard/grid.tsx
@@ -0,0 +1,16 @@
+import Footer from "../footer";
+import FormularioCostos from "./formulario_costo_flete";
+import { TablaCostoFlete } from "./tabla_costo_flete";
+
+export const Grid = () => {
+    return (
+        <>
+            <div className="px-4 grid gap-3 grid-cols-12">
+                <FormularioCostos />
+                <TablaCostoFlete />
+            </div>
+            <Footer />
+        </>
+
+    );
+};
