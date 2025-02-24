5dec091d0f9abfe89e5a55f424672cd060f990d5
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 16:27:30 -0600

    bg

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -21,8 +21,8 @@ export default function Login() {
 
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
-      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-stone-600 shadow-xl">
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-stone-800 bg-white px-4 py-6 pt-8 text-center sm:px-16">
+      <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-stone-400 bg-stone-800 shadow-xl">
+        <div className="flex flex-col items-center justify-center space-y-3 border-b border-stone-400 bg-stone-900 px-4 py-6 pt-8 text-center sm:px-16">
           <h3 className="text-xl font-semibold">Registro</h3>
           <p className="text-sm text-gray-300">
             Create an account with your email and password
