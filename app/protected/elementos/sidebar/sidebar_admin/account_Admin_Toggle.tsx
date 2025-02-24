2282df6c14e23fa791accf81c9a3e48cc498c419
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 11:13:28 -0600

    img

--- app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
+++ app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
@@ -6,19 +6,20 @@ export const AccountAdminToggle = async () => {
         <>
         <div className="border-b mb-4 mt-2 pb-4 border-stone-200">
             <button className="flex p-0.5 hover:bg-slate-600 rounded-full transition-colors relative gap-2 w-full item-center">
-                <img src="https://api.dicebear.com/9.x/notionists/svg" alt="avatar" className="size-8 rounded-full shrink-0 bg-sky-700 shadow"/>
+        
                 <div className="text-start">
                     <span className="text-sm font-bold block">
                         name
                     </span> 
-                </div>
-                 
-            </button>
-            <span className="text-xs block">
-                {session?.user?.email}
-            </span>
+                    <span className="text-xs block">
+                        {session?.user?.email}
+                    </span>
+                </div>         
+            </button> 
         </div>  
     </>
 
     );
 }
+
+//<img src="https://api.dicebear.com/9.x/notionists/svg" alt="avatar" className="size-8 rounded-full shrink-0 bg-sky-700 shadow"/>
