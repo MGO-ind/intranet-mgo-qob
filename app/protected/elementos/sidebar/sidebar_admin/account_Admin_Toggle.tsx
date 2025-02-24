9ced92044424b3fa01309a31e626b0ea1a289c2c
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 12:30:46 -0600

    flex

--- app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
+++ app/protected/elementos/sidebar/sidebar_admin/account_Admin_Toggle.tsx
@@ -5,14 +5,13 @@ export const AccountAdminToggle = async () => {
     return (
         <>
         <div className="border-b mb-4 mt-2 pb-4 border-stone-200">
-            <button className="flex items-center p-0.5 hover:bg-slate-600 rounded-full transition-colors relative gap-2 w-full item-center">
+            <button className="flex p-0.5 hover:bg-slate-600 rounded-full transition-colors relative gap-2 w-full item-center">
         
-                <div className="row-span-1 text-center">
+                <div className="text-center">
                     <span className="text-sm font-bold block">
                         name
                     </span> 
-                    </div>
-                    <div className="row-span-2 items-center text-center">
+                    
                     <span className="text-xs block">
                         {session?.user?.email}
                     </span>
