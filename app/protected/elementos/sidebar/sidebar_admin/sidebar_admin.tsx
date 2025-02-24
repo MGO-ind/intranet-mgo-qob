3b10214d7719031e55167e7d04c49c9b6c2ea70d
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-24 15:30:48 -0600

    route

--- app/protected/elementos/sidebar/sidebar_admin/sidebar_admin.tsx
+++ app/protected/elementos/sidebar/sidebar_admin/sidebar_admin.tsx
@@ -1,5 +1,6 @@
 import { Plan } from "../plan";
 import { AccountAdminToggle } from "./account_Admin_Toggle";
+import { RouteSelectAdmin } from "./routeSelect";
 
 
 
@@ -11,6 +12,7 @@ export default function SideBarAdmin() {
     <div className="font-[family-name:var(--font-geist-mono)]">
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
         <AccountAdminToggle/>
+        <RouteSelectAdmin/>
    
     </div>
     <Plan/>
