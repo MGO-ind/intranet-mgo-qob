b4459dce8788df2c8c9663926f99132d1c4422d4
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-25 09:02:23 -0600

    consulta fletes

--- app/protected/elementos/cotizador/grid_cotizador.tsx
+++ app/protected/elementos/cotizador/grid_cotizador.tsx
@@ -1,6 +1,6 @@
 import Footer from "../footer";
 import { CambioDolares } from "./cambio_dolares";
-import { ConsultaFlete } from "./consulta_fletes";
+
 import { FormularioCotizador } from "./formulario_cotizador";
 
 export const GridCotizador = () => {
@@ -8,7 +8,7 @@ export const GridCotizador = () => {
       
       <>
     <div className="px-4 grid gap-3 grid-cols-12">
-        <ConsultaFlete/>
+    
         <FormularioCotizador/>
         <CambioDolares/>
     </div>
