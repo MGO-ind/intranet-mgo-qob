7b7308f7193ae68ac4a08b928944cc50cc82af41
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-05 16:20:52 -0600

    espa;ol

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={GeistSans.variable}>{children}</body>
     </html>
   );
