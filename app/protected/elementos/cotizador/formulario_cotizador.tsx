74b76f21c2d870f16815f71d81db5cf57442b58b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-07 17:22:52 -0600

    check box

--- app/protected/elementos/cotizador/formulario_cotizador.tsx
+++ app/protected/elementos/cotizador/formulario_cotizador.tsx
@@ -252,7 +252,7 @@ export const FormularioCotizador = () => {
                         </label>
                     </div>
                     <div className="flex items-center ps-4 border border-gray-200 rounded-sm dark:border-gray-700">
-                        <label className="">
+                        
                             <input className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" 
                             name="tam" 
                             radioGroup="tamano"
@@ -261,8 +261,8 @@ export const FormularioCotizador = () => {
                             value={"Full"}
                             placeholder="Costo del Flete"
                             onChange={handleInputChange} 
-                            /> Full
-                        </label>
+                            /> 
+                            <label className="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Full </label>
                         </div>
                         <div>
                         <label className="">
