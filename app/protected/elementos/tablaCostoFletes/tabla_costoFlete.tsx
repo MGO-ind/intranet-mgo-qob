625e6bda85274ef014e373af40456c8dd074c102
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-10 10:52:13 -0600

    costo

---
+++ app/protected/elementos/tablaCostoFletes/tabla_costoFlete.tsx
@@ -0,0 +1,37 @@
+import {dbCosto, costoflete} from 'app/schema'
+import RefreshButton from './refresh_button';
+export default async function TableFlete() {
+    let fletes: any[] = []
+    try {
+        fletes = await dbCosto.select().from(costoflete);
+    } catch (e: any) {
+        console.error(e);
+    }
+    return (
+        <>
+        <RefreshButton/>
+        <table className="table-auto w-full">
+            <thead>
+                <tr>
+                    <th>Origen</th>
+                    <th>Destino</th>
+                    <th>Talla de envio</th>
+                    <th>Costo</th>
+                    <th>Paqueteria</th>
+                </tr>
+            </thead>
+            <tbody>
+                {fletes && fletes.map((flete: any) => (
+                    <tr key={flete.id}>
+                        <td>{flete.origen}</td>
+                        <td>{flete.destino}</td>
+                        <td>{flete.tallaenvio}</td>
+                        <td>{flete.costo}</td>
+                        <td>{flete.id_paqueteria}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </>
+    )
+}
