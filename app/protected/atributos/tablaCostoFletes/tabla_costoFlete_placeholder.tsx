4ba438d0e54fd09a7c97702a27b7e38bfabe0908
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-11 17:29:56 -0600

    asd

--- app/protected/atributos/tablaCostoFletes/tabla_costoFlete_placeholder.tsx
+++ app/protected/atributos/tablaCostoFletes/tabla_costoFlete_placeholder.tsx
@@ -8,7 +8,6 @@ export async function TablaCostoFleteFull() {
         <div className="lg:col-span-12 sm:col-span-12 rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
-
                     Fletes
                 </h3>
                 <h3 className="font-normal">
