74fa9c1aa9c3cce80631d1fd7d239a3ccc36419c
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-12 11:37:50 -0600

    estilos

--- app/protected/atributos/cotizador/formulario_cotizador.tsx
+++ app/protected/atributos/cotizador/formulario_cotizador.tsx
@@ -273,8 +273,10 @@ export const FormularioCotizador = () => {
                             />
                        
                     </div>
-                    <div className="flex items-center ps-2 border border-gray-200 rounded-sm dark:border-gray-700">
-                        
+                    
+                    <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                      <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">  
+                      <div className="flex items-center ps-3">
                         <input className="w-4 h-4 text-stone-600 bg-gray-100 border-gray-300 focus:ring-stone-500 dark:focus:ring-stone-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" 
                             name="tam" 
                             radioGroup="tamano"
@@ -285,9 +287,10 @@ export const FormularioCotizador = () => {
                             onChange={handleInputChange} 
                         /> 
                         <label className="w-10 py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Full </label>
-                    </div>
-                    <div className="flex items-center ps-2 border border-gray-200 rounded-sm dark:border-gray-700">
-                        
+                        </div>
+                        </li>
+                        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">  
+                        <div className="flex items-center ps-2 border border-gray-200 rounded-sm dark:border-gray-700">
                         <input className="w-4 h-4 text-stone-600 bg-gray-100 border-gray-300 focus:ring-stone-500 dark:focus:ring-stone-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" 
                             name="tam" 
                             type="radio"
@@ -298,7 +301,9 @@ export const FormularioCotizador = () => {
                             onChange={handleInputChange} 
                             />
                         <label className="w-10 py-2 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> Jumbo</label>
-                    </div>
+                        </div>
+                        </li>
+                        </ul>
 
                     <button className="rounded-full border-green-300 border  transition-colors hover:bg-green-500 dark:bg-green-700 text-sm sm:text-base h-8 w-full sm:h-10 px-2 sm:px-5 m-2" 
                         type="submit">Calcular</button>
