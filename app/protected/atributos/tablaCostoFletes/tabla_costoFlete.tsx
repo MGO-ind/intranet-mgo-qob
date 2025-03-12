688d9e13248d760114c7d108313c6007b84d9e55
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-12 12:20:26 -0600

    dfg

--- app/protected/atributos/tablaCostoFletes/tabla_costoFlete.tsx
+++ app/protected/atributos/tablaCostoFletes/tabla_costoFlete.tsx
@@ -46,13 +46,7 @@ export default async function TableFlete() {
                     </button>
                     <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
                         1
-                    </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        2
-                    </button>
-                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        3
-                    </button>
+                    </button>                   
                     <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                         Sig
                     </button>
