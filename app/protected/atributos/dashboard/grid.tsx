810acb68840c487cf81cf06cee0de58c532b6054
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-12 14:16:51 -0600

    321

--- app/protected/atributos/dashboard/grid.tsx
+++ app/protected/atributos/dashboard/grid.tsx
@@ -1,13 +1,11 @@
 import Footer from "../footer";
-import FormularioCostos from "../registro_fletes/place_formulario";
-import { TablaCostoFlete } from "../registro_fletes/tabla_costo_flete";
+
 
 export const Grid = () => {
     return (
         <>
             <div className="px-4 grid gap-3 grid-cols-12">
-                <FormularioCostos />
-                <TablaCostoFlete />
+            
             </div>
             <Footer />
         </>
---
+++ app/protected/registro_flete/page.tsx
@@ -0,0 +1,13 @@
+import { RegistroFletes } from "../atributos/registro_fletes/registro_fletes";
+import SideBarAdmin from "../atributos/sidebar/sidebar_admin/sidebar_admin";
+
+
+export default function Home() {
+    return (
+      
+      <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
+        <SideBarAdmin/>
+        <RegistroFletes/>
+      </main>   
+    );
+  }
