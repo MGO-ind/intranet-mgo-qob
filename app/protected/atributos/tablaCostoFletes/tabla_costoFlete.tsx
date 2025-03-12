bd1125d7862aa8620aa2ebcc91cad6f5a81b5ab1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-12 12:06:47 -0600

    paginacion

--- app/protected/atributos/tablaCostoFletes/tabla_costoFlete.tsx
+++ app/protected/atributos/tablaCostoFletes/tabla_costoFlete.tsx
@@ -36,6 +36,28 @@ export default async function TableFlete() {
                         ))}
                     </tbody>
                 </table>
+                <div className="flex justify-between items-center px-4 py-3">
+                    <div className="text-sm text-slate-500">
+                    Mostrando <b>1-5</b> de 45
+                    </div>
+                    <div className="flex space-x-1">
+                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                        Prev
+                    </button>
+                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
+                        1
+                    </button>
+                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                        2
+                    </button>
+                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                        3
+                    </button>
+                    <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+                        Sig
+                    </button>
+                    </div>
+                </div>
             </div>
         </>
     );
