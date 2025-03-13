0fe4199a2a330d4c3e76c9a6f182d030d210b10d
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-13 09:30:53 -0600

    prueba 1 de rutas

--- app/protected/atributos/sidebar/sidebar_admin/routeSelect.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/routeSelect.tsx
@@ -9,35 +9,29 @@ import Link from "next/link";
 export const RouteSelectAdmin = () => {
   return (
     <div className="space-y-1">
-      <TransitionLink Icon={FiHome} selected={false} title="Inicio" href="/"/>
-      <TransitionLink Icon={FiPaperclip} selected={false} title="Registrar Flete" href="/protected/registro_flete"/>
-      <TransitionLink Icon={FiDollarSign} selected={false} title="Cotizador" href="/protected/cotizador" />
-      <TransitionLink Icon={GrDeliver} selected={false} title="Consultar Fletes" href="/protected/consultar_flete" />
-      <TransitionLink Icon={MdOutlineInventory} selected={false} title="Inventario" href="/protected/inventario" />
+      <TransitionLink Icon={FiHome} title="Inicio" href="/"/>
+      <TransitionLink Icon={FiPaperclip} title="Registrar Flete" href="/protected/registro_flete"/>
+      <TransitionLink Icon={FiDollarSign} title="Cotizador" href="/protected/cotizador" />
+      <TransitionLink Icon={GrDeliver} title="Consultar Fletes" href="/protected/consultar_flete" />
+      <TransitionLink Icon={MdOutlineInventory} title="Inventario" href="/protected/inventario" />
     </div>
   );
 };
 
 const TransitionLink = ({
-  selected,
   Icon,
   title,
   href,
 }: {
-  selected: boolean;
   Icon: IconType;
   title: string;
   href: string;
 }) => {
   return (
     <Link href={href}
-      className={`flex items-center justify-start gap-2 w-full rounded-full px-2 py-1.5 text-sm text-stone-50 transition-[box-shadow,_background-color,_color] ${
-        selected
-          ? "bg-slate-700 shadow"
-          : "hover:bg-slate-200 bg-transparent hover:text-stone-900 shadow-none"
-      }`}
+      className={`flex items-center justify-start gap-2 w-full rounded-full px-2 py-1.5 text-sm text-stone-50 transition-[box-shadow,_background-color,_color] hover:bg-slate-200 bg-transparent hover:text-stone-900 shadow-none `}
     >
-      <Icon className={selected ? "text-blue-400" : ""} />
+      <Icon />
       <span>{title}</span>
     </Link>
   );
--- app/schema.ts
+++ app/schema.ts
@@ -74,51 +74,52 @@ export const dbCosto = drizzle(client)
   });
 */
 
-export async function getDatosEmpleado(costo: string) {
-  const costofletes = await ensureTableFleteExists();
-  return await db.select().from(costofletes).where(eq(costofletes.costo, costo));
+export async function getDatosEmpleado(correo: string) {
+  const datosEmpleado = await ensureTableDatosEmpleadoExists();
+  return await db.select().from(datosEmpleado).where(eq(datosEmpleado.correo, correo));
 }
 
 
-export async function createDatosEpleado(origen: string, destino: string, tallaenvio: string, costo1: number, id_paqueteria1: number) {
-  const costofletes = await ensureTableFleteExists();
-  const costo = costo1.toString();
-  const id_paqueteria = id_paqueteria1.toString();
-  return await db.insert(costofletes).values([{ origen, destino, tallaenvio, costo, id_paqueteria}]);
+export async function createDatosEpleado(nombre: string, apellido: string, correo: string) {
+  const datosEmpleado = await ensureTableDatosEmpleadoExists();
+  return await db.insert(datosEmpleado).values([{ nombre, apellido, correo}]);
 }
 
 
-async function ensureTableDatosEmpleados() {
+async function ensureTableDatosEmpleadoExists() {
   const result = await client`
     SELECT EXISTS (
       SELECT FROM information_schema.tables 
       WHERE table_schema = 'public' 
-      AND table_name = 'costofletes'
+      AND table_name = 'datosEmpleado'
     );`;
 
   if (!result[0].exists) {
     await client`
-      CREATE TABLE "costofletes" (
+      CREATE TABLE "datosEmpleado" (
         id SERIAL PRIMARY KEY,
-        origen TEXT,
-        destino TEXT,
-        tallaenvio TEXT,
-        costo numeric,
-        id_paqueteria numeric
+        nombre TEXT,
+        apellido TEXT,
+        correo TEXT
       );`;
   }
 
-  const table = pgTable('costofletes', {
+  const table = pgTable('datosEmpleado', {
     id: serial('id').primaryKey(),
-    origen: text('origen'),
-    destino: text('destino'),
-    tallaenvio: text('tallaenvio'),
-    costo: numeric('costo'),
-    id_paqueteria: numeric('id_paqueteria'),
+    nombre: text('origen'),
+    apellido: text('destino'),
+    correo: text('tallaenvio'),
   });
 
   return table;
 }
+
+export const datosEmpleado = pgTable('datosEmpleado', {
+  id: serial('id').primaryKey(),
+  nombre: text('origen'),
+  apellido: text('destino'),
+  correo: text('tallaenvio'),
+});
 
 
 
