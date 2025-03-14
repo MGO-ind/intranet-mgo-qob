f35adb431c576f67331e5f073535e419b5771f6b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-14 09:29:10 -0600

    console

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -6,6 +6,7 @@ export default async function ProtectedPage() {
   let session = await auth();
   let correo = session?.user?.email;
   console.log(correo);
+  console.log("lkdlakjds");
 
   return (
     <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
