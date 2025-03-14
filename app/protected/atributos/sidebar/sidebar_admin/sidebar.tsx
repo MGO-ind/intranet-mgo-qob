ff6b53b02ae242868c67e7136b012a63d8059190
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-14 15:46:11 -0600

    hi

--- app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/sidebar.tsx
@@ -5,7 +5,7 @@ import { RouteSelectAdmin } from "./routeSelectAdmin";
 export function SideBarAdmin(){
 
   return (
-    <div className="font-[family-name:var(--font-geist-mono)] sm:w-[100px]">
+    <div className="font-[family-name:var(--font-geist-mono)] w-[200px] sm:w-[100px]">
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
         <AccountAdminToggle/>
         <RouteSelectAdmin/> 
