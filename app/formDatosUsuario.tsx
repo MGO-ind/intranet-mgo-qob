c760a524c3407d8c86900f269219dae2bacd712b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-14 13:05:32 -0600

    zxc

--- app/formDatosUsuario.tsx
+++ app/formDatosUsuario.tsx
@@ -39,8 +39,7 @@ export function FormDatosUsuario({
             required
             className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
           />
-        </div>  
-        <SelectCorreos/>   
+        </div>     
         <div>
           <label
             htmlFor="nivel"
@@ -68,6 +67,7 @@ export function FormDatosUsuario({
           >
             Correo
           </label>
+          <SelectCorreos/>
           <input
             id="correo"
             name="correo"
