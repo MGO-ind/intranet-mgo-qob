a37649f483d03caf71a2003258329438b0a2175b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-14 09:38:34 -0600

    fetch

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,14 +5,23 @@ import { SideBarAdmin } from './atributos/sidebar/sidebar_admin/sidebar';
 export default async function ProtectedPage() {
   let session = await auth();
   let correo = session?.user?.email;
-  console.log(correo);
-  console.log("lkdlakjds");
+  let usuarios: any[] = [];
+  try {
+    const response = await fetch('/api/usuarios');
+    usuarios = await response.json();
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+  
 
   return (
     <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
     <SideBarAdmin/>
+
     <Dashboard />
-    {correo}
+    {usuarios}
 
   </main> 
   );
