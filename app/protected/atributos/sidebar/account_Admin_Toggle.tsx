0e9597e97ccdeac31d1816d267803f925213b733
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-14 12:23:00 -0600

    barra top

--- app/protected/atributos/sidebar/account_Admin_Toggle.tsx
+++ app/protected/atributos/sidebar/account_Admin_Toggle.tsx
@@ -1,7 +1,28 @@
 import { auth } from 'app/auth';
+import { getUsuario } from "@/app/schema";
 
 export const AccountAdminToggle = async () => {
     let session = await auth();
+
+    let correo = session?.user?.email;
+    let usuarios: any[] = [];
+    let nameUsuario: string | undefined;
+    
+    try {
+      correo?.toString();
+      if (correo) {
+        const usuarioResponse = await getUsuario(correo);
+        usuarios = usuarioResponse;
+  
+        if (usuarios.length > 0) {
+          nameUsuario = usuarios[0].nombre; // Asignar el nivel del primer usuario a la variable
+        }
+      }
+  
+    }
+    catch (error) {
+      console.error(error);
+    }
     return (
         <>
         <div className="border-b mb-4 mt-2 pb-4 border-stone-200">
@@ -9,7 +30,7 @@ export const AccountAdminToggle = async () => {
         
                 <div className="text-center px-2">
                     <span className="text-sm font-bold block">
-                        name
+                        {nameUsuario} 
                     </span> 
                     
                     <span className="text-xs block py-1">
