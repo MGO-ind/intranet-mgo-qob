209fe7119957e3f4661cfbee6c3e91d78f4dd678
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-03-31 13:20:06 -0600

    correo

--- app/protected/atributos/inputCorreoUsuario.tsx
+++ app/protected/atributos/inputCorreoUsuario.tsx
@@ -6,11 +6,12 @@ export const InputCorreoUsuario = async () => {
   return ( 
 
     <input
-    id="correo"
-    name="correo"
+    id="correo_empleado"
+    name="correo_empleado"
     value={session?.user?.email ?? ''}
     required
     readOnly
+    type="email"
     className="mt-1 block w-full text-black rounded-full border border-gray-700 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
 />
    
