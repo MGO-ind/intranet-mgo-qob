2aab3371846e34b24feff098ab34eceb97a92295
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-01 15:23:50 -0600

    icono

--- app/protected/atributos/registro_productos/place_formulario_producto.tsx
+++ app/protected/atributos/registro_productos/place_formulario_producto.tsx
@@ -3,7 +3,7 @@ import { FormRegistroProductos } from "@/app/formProducto";
 import { createNewProduct, getProducto } from "@/app/schema";
 import { SubmitButton } from "@/app/submit-button";
 import { redirect } from "next/navigation";
-import { FiFolder } from "react-icons/fi";
+import { AiOutlineTag } from "react-icons/ai";
 
 
 export default function FormularioProductos() {
@@ -35,7 +35,7 @@ export default function FormularioProductos() {
         <div className="lg:col-span-6 sm:col-span-12 rounded-3xl bg-zinc-800 border border-stone-500 shadow-lg h-[auto] sm:h-auto overflow-y-scroll scrollbar-thin">
             <div className="p-4">
                 <h3 className="flex items-center text-lg gap-1.5 font-medium">
-                    <FiFolder />
+                    <AiOutlineTag />
                     Productos
                 </h3>
                 <h3 className="font-normal">
