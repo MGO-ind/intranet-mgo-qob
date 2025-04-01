6df912b7b91ffece1f7042866dc7852436083620
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-04-01 11:13:52 -0600

    registrar clientes

--- app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
+++ app/protected/atributos/sidebar/sidebar_admin/routeSelectAdmin.tsx
@@ -19,8 +19,10 @@ export const RouteSelectAdmin = () => {
       <TransitionLink Icon={GrDeliver} title="Consultar Fletes" href="/protected/consultar_flete" />
       <TransitionLink Icon={MdOutlineInventory} title="Inventario" href="/protected/inventario" />
       <TransitionLink Icon={FaPen} title="Datos Usuario" href="/protected/registro_datos_usuario" />
-      <TransitionLink Icon={FaRegPenToSquare} title="Registro Producto" href="/protected/registro_productos" />
+      <TransitionLink Icon={FaRegPenToSquare} title="Registro Productos" href="/protected/registro_productos" />
       <TransitionLink Icon={LuNotebookText } title="Catalogo de Productos" href="/protected/catalogo_productos" />
+      <TransitionLink Icon={FaRegPenToSquare} title="Registro Clientes" href="/protected/registro_clientes" />
+      <TransitionLink Icon={LuNotebookText } title="Catalogo de Clientes" href="/protected/catalogo_productos" />
     </div>
   );
 };
